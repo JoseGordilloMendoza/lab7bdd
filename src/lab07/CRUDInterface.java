15caf11dfd50961ecec1c5d772cdd6340b415cb8
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2024-06-19 23:50:12 -0500

    Ahora se muestran mensajes cuando algo no es debido, tambien el boton actualizar es mas util y entendible de usar

--- src/lab07/CRUDInterface.java
+++ src/lab07/CRUDInterface.java
@@ -160,7 +160,7 @@ public class CRUDInterface extends JFrame {
 
     private void modificar() {
         int selectedRow = table.getSelectedRow();
-        if (selectedRow != -1 && !tableModel.getValueAt(selectedRow, 2).toString().equals("*")) {
+        if (selectedRow != -1 && tableModel.getValueAt(selectedRow, 2).toString().equals("A")) {
             txtCodigo.setText(tableModel.getValueAt(selectedRow, 0).toString());
             txtNombre.setText(tableModel.getValueAt(selectedRow, 1).toString());
             lblEstado.setText(tableModel.getValueAt(selectedRow, 2).toString());
@@ -169,22 +169,33 @@ public class CRUDInterface extends JFrame {
             operation = "mod";
             btnActualizar.setEnabled(true);
         }
+        else if (tableModel.getValueAt(selectedRow, 2).toString().equals("I")){
+            JOptionPane.showMessageDialog(this, "El registro se encuentra inactivo", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        else if (tableModel.getValueAt(selectedRow, 2).toString().equals("*")){
+            JOptionPane.showMessageDialog(this, "El registro esta eliminado", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     private void eliminar() {
         int selectedRow = table.getSelectedRow();
-        if (selectedRow != -1 && !tableModel.getValueAt(selectedRow, 2).toString().equals("*")) {
-        int confirmacion = JOptionPane.showConfirmDialog(this, "¿Estás seguro de que deseas eliminar este registro?", "Confirmar eliminación", JOptionPane.YES_NO_OPTION);
-        if (confirmacion == JOptionPane.YES_OPTION) {
-            txtCodigo.setText(tableModel.getValueAt(selectedRow, 0).toString());
-            txtNombre.setText(tableModel.getValueAt(selectedRow, 1).toString());
-            lblEstado.setText("*");
-            operation = "mod";
-            CarFlaAct = 1;
-            btnActualizar.setEnabled(true);
-            actualizar();
+        if (selectedRow != -1) {
+            if(tableModel.getValueAt(selectedRow, 2).toString().equals("A") || tableModel.getValueAt(selectedRow, 2).toString().equals("I")){
+                int confirmacion = JOptionPane.showConfirmDialog(this, "¿Estás seguro de que deseas eliminar este registro?", "Confirmar eliminación", JOptionPane.YES_NO_OPTION);
+                if (confirmacion == JOptionPane.YES_OPTION) {
+                    txtCodigo.setText(tableModel.getValueAt(selectedRow, 0).toString());
+                    txtNombre.setText(tableModel.getValueAt(selectedRow, 1).toString());
+                    lblEstado.setText("*");
+                    operation = "mod";
+                    CarFlaAct = 1;
+                    btnActualizar.setEnabled(true);
+                    actualizar();
+                }
+            }
+            else {
+            JOptionPane.showMessageDialog(this, "El registro esta eliminado", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
-    }
     }
 
     private void inactivar() {
@@ -214,6 +225,8 @@ public class CRUDInterface extends JFrame {
             CarFlaAct = 1;
             operation="mod";
             btnActualizar.setEnabled(true);
+            txtCodigo.setEditable(false);
+            txtNombre.setEditable(false);
         }
         else if (tableModel.getValueAt(selectedRow, 2).toString().equals("A")){
             JOptionPane.showMessageDialog(this, "El registro ya se encuentra activo", "Error", JOptionPane.ERROR_MESSAGE);
